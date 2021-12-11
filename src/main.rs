18888ef56eb5d1f66f1e911935800eebbb2ef45e
ssheldon10 <ssheldon@umd.edu>
2021-12-11 18:23:18 -0500

    Added analysis method and commented code

--- src/main.rs
+++ src/main.rs
@@ -75,30 +75,43 @@ async fn main() {
         }
     }
 
+    // Display final comment before pausing to wait
     print!("Great thanks! We will generate that report when you requested :)\n\n");
 
 
 
-    //let cityname = &city;
+    // Create thte location object based on information provided by user
     let loc = LocationSpecifier::CityAndCountryName{city, country};
-    let weather = openweather::get_current_weather(&loc, API_KEY, &Settings::default()).unwrap();
 
+    // Initialize Twilio app to send the user's message
     let app_id = "ACd52baed8b7aa3964748b11a1c2476305";
     let auth_token = "64e0fde8442f7a0b0447899527e67b97";
     let client = Client::new(app_id, auth_token);
     let from = "17258884016";
 
+    // Wait the specified amount of time
     let mut child = Command::new("sleep").arg((min_amt*60.0).to_string()).spawn().unwrap();
     let _result = child.wait().unwrap();
     
+    // Get the current weather data and generate the report
+    let weather = openweather::get_current_weather(&loc, API_KEY, &Settings::default()).unwrap();
     let body = analyzeweather(weather);
+
+    // Print the report to stdout
     print!("{}\n", body);
 
+    // Send the report via twilio to the phone number
     //let msg = OutboundMessage::new(from, &number, &body);
     //client.send_message(msg).await; 
 }
 
 
+/**
+ * Function to generate a randomized report of the current weather status.
+ * Weather information is provided via a WeatherReportCurrent object from the openweather crate
+ * Rain quantity, snow quantity, humidity %, wind speed, and cloud cover % are all used
+ * returns a String type
+ */
 fn analyzeweather(weather: openweather::WeatherReportCurrent) -> String {
     let min = convert_to_f(weather.main.temp_min);
     let max = convert_to_f(weather.main.temp_max);
@@ -108,6 +121,9 @@ fn analyzeweather(weather: openweather::WeatherReportCurrent) -> String {
 
     // Add randomized report based on rain status and amounts
     report.push_str(&gen_rain_report(&weather));
+
+    // Add randomized report based on humidity
+    report.push_str(&gen_humidity_report(&weather));
 
     // Add randomized report based on snow status and amounts
     report.push_str(&gen_snow_report(&weather));
@@ -123,7 +139,11 @@ fn analyzeweather(weather: openweather::WeatherReportCurrent) -> String {
 
 }
 
-
+/**
+ * Uses WeatherReportCurrent object to generate a random one line report based off of
+ * the current rain quantities.
+ * returns a String with a new line at the end
+ */
 fn gen_rain_report (weather: &openweather::WeatherReportCurrent) -> String {
     let mut rng = rand::thread_rng();
     if weather.rain != None {
@@ -174,6 +194,11 @@ fn gen_rain_report (weather: &openweather::WeatherReportCurrent) -> String {
     }
 }
 
+/**
+ * Uses WeatherReportCurrent object to generate a random one line report based off of
+ * the current wind speed.
+ * returns a String with a new line at the end
+ */
 fn gen_wind_report(weather:&openweather::WeatherReportCurrent) -> String {
     let wind_val = weather.wind.speed * 2.237;
     let mut rng = rand::thread_rng();
@@ -211,7 +236,52 @@ fn gen_wind_report(weather:&openweather::WeatherReportCurrent) -> String {
     wind_str
 }
 
+/**
+ * Uses WeatherReportCurrent object to generate a random one line report based off of
+ * the current humidity percentage.
+ * returns a String with a new line at the end
+ */
+fn gen_humidity_report(weather: &openweather::WeatherReportCurrent) -> String {
+    let humid_val = weather.main.humidity;
+    let mut rng = rand::thread_rng();
+    let humid_str = {
+        if humid_val <= 0.25 {
+            // Dry
+            match rng.gen_range(0..4) {
+                0 => "It's a dry one out there today, quite low humidity.\n",
+                _ => "",
+            }
+        } else if humid_val <= 0.50 {
+            // Comfy
+            match rng.gen_range(0..3) {
+                0 => "The air should be comfy today, not too humid.\n",
+                _ => "",
+            }
+        } else if humid_val <= 0.75 {
+            // Getting sticky
+            match rng.gen_range(0..5) {
+                0 => "It is getting sticky out there as the humidity picks up\n",
+                1 => "Let's hope it rains soon, I can't take this humidity\n",
+                2  => "Don't even bother straightening your hair, it won't last in this humidity",
+                _ => "",
+            }
+        } else {
+            // Sky will open up
+            match rng.gen_range(0..3) {
+                0 => "With humidity this high, it is bound to rain at any second!\n",
+                1 => "Might want to pack an umbrella juuust in case. This humidity means a storm's brewin\n",
+                _ => "",
+            }
+        }
+    };
+    String::from(humid_str)
+}
 
+/**
+ * Uses WeatherReportCurrent object to generate a random one line report based off of
+ * the current cloud cover percentage
+ * returns a String with a new line at the end
+ */
 fn gen_cloud_report(weather: &openweather::WeatherReportCurrent) -> String {
     let cloud_val = weather.clouds.all;
     let mut rng = rand::thread_rng();
@@ -219,25 +289,25 @@ fn gen_cloud_report(weather: &openweather::WeatherReportCurrent) -> String {
     let cloud_str = {
         if cloud_val <= 25 {
             // Sunny Skies!
-            match rng.gen_range(0..2) {
+            match rng.gen_range(0..3) {
                 0 => "Skies are looking sunny, maybe pack some shades\n",
                 1 => "Not a cloud in sight, enjoy the blue skies\n",
                 _ => "",
             }
         } else if cloud_val <= 50 {
-            match rng.gen_range(0..2) {
+            match rng.gen_range(0..3) {
                 0 => "Just a few clouds in the sky today\n",
                 1 => "Great day for some cloud watching, not too many not too little\n",
                 _ => "",
             }
         } else if cloud_val <= 75 {
-            match rng.gen_range(0..2) {
+            match rng.gen_range(0..3) {
                 0 => "Pretty cloudy out there today, time to ditch the shades\n",
                 1 => "You won't be needing any visors with clouds like these today\n",
                 _ => "",
             }
         } else {
-            match rng.gen_range(0..2) {
+            match rng.gen_range(0..3) {
                 0 => "It'll be a dark one out there today with all this cloud cover\n",
                 1 => "Might want to pack a flashlight, these clouds mean it's gonna be dark\n",
                 _ => "Seems like a storm's brewing with such high cloud cover!\n",
@@ -248,6 +318,11 @@ fn gen_cloud_report(weather: &openweather::WeatherReportCurrent) -> String {
     String::from(cloud_str)
 }
 
+/**
+ * Uses WeatherReportCurrent object to generate a random one line report based off of
+ * the current snow quantities.
+ * returns a String with a new line at the end
+ */
 fn gen_snow_report(weather: &openweather::WeatherReportCurrent) -> String {
     let mut rng = rand::thread_rng();
     if weather.snow != None {
@@ -274,7 +349,7 @@ fn gen_snow_report(weather: &openweather::WeatherReportCurrent) -> String {
                         _ => String::from("Make sure to pack a jacket and an wear boots today because the heavy snow is no joke\n"),
                     }
                 } else {
-                    match rng.gen_range(0..3) {
+                    match rng.gen_range(0..4) {
                         0 => format!("Whoa! Crazy heavy snow today clocking in at {:.0}mm per hour!\n", amt),
                         1 => String::from("This snow is almost a blizzard! I'd stay home if I were you\n"),
                         2 => String::from("Be safe out there today, heavy snow is coming your way!!\n"),
@@ -305,6 +380,9 @@ fn gen_snow_report(weather: &openweather::WeatherReportCurrent) -> String {
     }
 }
 
+/**
+ * Converts kelvin temperature to fahrenheit
+ */
 fn convert_to_f(temp: f32) -> f32 {
     return (temp - 273.15) * (9.0/5.0) + 32.0;
 }
